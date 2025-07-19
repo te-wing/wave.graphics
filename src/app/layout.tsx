09f37233cec490c37dce249f7586b1e969d5e436
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-19 12:35:15 +0000

    タイトル

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.scss";
 import styles from './rootLayout.module.scss';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: "Wave | Physics Simulator App by Wing",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
+        <h1><Link href='/' className={styles.titleLink}>Wave</Link> by Wing</h1>
         {children}
       </body>
     </html>
