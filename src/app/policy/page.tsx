ca58c40ee7a8593b6a848dff7a1a44f12165bb61
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-20 11:53:10 +0000

    規約一覧　作り始めました

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 export default function policyPage() {
   return (
     <>
-      <p>すみません，準備中です...</p>
+      <h3>ご利用規約等一覧</h3>
+      <p>
+        このアプリをご利用になる方は，以下の規約に同意していただく必要があります．必ずご覧ください．<br />
+        なお，この規約の適用範囲は，<Link href='/'>wave.app.wing.osaka</Link>内のコンテンツに限定されます．
+      </p>
     </>
   )
 }
