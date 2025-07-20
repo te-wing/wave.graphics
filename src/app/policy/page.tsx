2d730a490da4295cc7550b636cf361bd90a7c0e1
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-20 12:41:13 +0000

    リスト完了

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import styles from './policyList.module.scss';
 
 export default function policyPage() {
   return (
@@ -8,6 +9,25 @@ export default function policyPage() {
         このアプリをご利用になる方は，以下の規約に同意していただく必要があります．必ずご覧ください．<br />
         なお，この規約の適用範囲は，<Link href='/'>wave.app.wing.osaka</Link>内のコンテンツに限定されます．
       </p>
+      <section>
+        <table>
+          <tr>
+            <td><Link href='/policy/privacy'>プライバシーに関する声明</Link></td>
+            <td>：</td>
+            <td>当サイト及び運営者が取得する情報について</td>
+          </tr>
+          <tr>
+            <td><Link href='/policy/privacy'>ご利用規約</Link></td>
+            <td>：</td>
+            <td>ご利用頂くための規則および確認事項</td>
+          </tr>
+          <tr>
+            <td><Link href='/policy/privacy'>複製・転載・改変について</Link></td>
+            <td>：</td>
+            <td>当サイトの複製・転載・改変の可否について</td>
+          </tr>
+        </table>
+      </section>
       <h3>連絡先</h3>
       <p>このサイトに関して何らかのトラブルが起きた場合で，特に解決を希望される場合，フィードバックは，<a href='mailto:wing@me.wing.osaka'>wing@me.wing.osaka</a>までご連絡ください．反応するかどうかは，知りません．</p>
     </>
