64541ae868ed77506c5ffabc01bbb1196cf232dc
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-20 11:57:16 +0000

    連絡先

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -8,6 +8,8 @@ export default function policyPage() {
         このアプリをご利用になる方は，以下の規約に同意していただく必要があります．必ずご覧ください．<br />
         なお，この規約の適用範囲は，<Link href='/'>wave.app.wing.osaka</Link>内のコンテンツに限定されます．
       </p>
+      <h3>連絡先</h3>
+      <p>このサイトに関して何らかのトラブルが起きた場合で，特に解決を希望される場合，フィードバックは，<a href='mailto:wing@me.wing.osaka'>wing@me.wing.osaka</a>までご連絡ください．反応するかどうかは，知りません．</p>
     </>
   )
 }
