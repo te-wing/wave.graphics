a1d8572c264936079664a3368c28ecd34739c50c
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 00:17:00 +0000

    ホームボタン

---
+++ src/app/wavelayout/layout.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import styles from './wavelayout.module.scss';
+
+export default function WaveLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className={styles.homeButton}>
+        <Link className={styles.homeLink} href='/'>
+          <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill='currentColor'><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
+        </Link>
+      </div>
+      {children}
+    </>
+  );
+}
