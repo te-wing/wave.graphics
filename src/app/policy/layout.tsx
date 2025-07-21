a77b452f3bb32e24db513468d7c33de16671247d
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 00:29:26 +0000

    ホームボタン

--- src/app/policy/layout.tsx
+++ src/app/policy/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
+import styles from './policylayout.module.scss';
 
 export const metadata: Metadata = {
   title: 'ご利用規約一覧 | Wave App by Wing',
@@ -14,6 +16,11 @@ export default function PolicyLayout({
     <>
       <h2>ご利用規約</h2>
       {children}
+      <div className={styles.homeButton}>
+        <Link className={styles.homeLink} href='/'>
+          <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill='currentColor'><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
+        </Link>
+      </div>
     </>
   );
 }
