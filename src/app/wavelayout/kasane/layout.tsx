dec9c815806e6ed8e2c3ae5a63f6140c777c27c4
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 23:55:49 +0000

    更新完了！

--- src/app/wavelayout/kasane/layout.tsx
+++ src/app/wavelayout/kasane/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const metadata: Metadata = {
   title: '波の重ね合わせ | Wave App by Wing',
-  description: '先ほどのパルス波を使って，二つの波が重なったらどうなるか，観察してみましょう．'
+  description: 'パルス波を使って，二つの波が重なったらどうなるか，観察してみましょう．'
 };
 
 export default function SuperpositionWaveLayout({
