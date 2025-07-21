e11c5d19b4a7e26cb9acfa9c35a7de292718f503
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 06:44:29 +0000

    home完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,16 @@ export default function Home() {
     <>
       <p>スクロールして，様々な波の模型を見てみましょう！</p>
       <table>
+        <colgroup>
+          <col className={styles.col1}/>
+          <col />
+          <col />
+        </colgroup>
         <tbody>
           <tr>
             <td><Link className={styles.waveList} href='/wavelayout/tanshindou'>単振動</Link></td>
-            <td>単振動は，波の</td>
+            <td>：</td>
+            <td>単振動は，波の基本です．さっそく可視化してみましょう．</td>
           </tr>
         </tbody>
       </table>
