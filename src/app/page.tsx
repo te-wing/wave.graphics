388a46f69cdc38fbe6bc58c87ef518b1a884eaf9
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 22:26:41 +0000

    ほい

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,9 +44,9 @@ export default function Home() {
       <p>本サイトのソースコードは，全て<a href='https://r.wing.osaka/wavesrccode' target='_blank'>こちら</a>で公開しております．</p>
       <h3>利用したフレームワーク</h3>
       <ul>
-        <li><a href='https://r.wing.osaka/nextjs' target='_blank'>Next.js</a></li>
-        <li><a href='https://r.wing.osaka/katex' target='_blank'>KaTeX</a></li>
-        <li><a href='https://r.wing.osaka/sass' target='_blank'>Sass</a></li>
+        <li><strong><a href='https://r.wing.osaka/nextjs' target='_blank'>Next.js</a></strong>：Webアプリの構築を効率化し，高速で使いやすくする．</li>
+        <li><strong><a href='https://r.wing.osaka/katex' target='_blank'>KaTeX</a></strong>：Webページ上に数式を記述できる．</li>
+        <li><strong><a href='https://r.wing.osaka/sass' target='_blank'>Sass</a></strong>：CSSを拡張したスタイルシート言語．</li>
       </ul>
     </>
   );
