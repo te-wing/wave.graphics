ec510760b3e2da01e8b9691f07fefe73a04a47e6
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 06:39:41 +0000

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+import styles from './homepage.module.scss';
+
 export default function Home() {
   return (
     <>
       <p>スクロールして，様々な波の模型を見てみましょう！</p>
+      <table>
+        <tbody>
+          <tr>
+            <td><Link className={styles.waveList} href='/wavelayout/tanshindou'>単振動</Link></td>
+            <td>単振動は，波の</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
