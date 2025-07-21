ccd105e16fb05d1c32d5e957866b44499ce7530b
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 06:24:38 +0000

    ほい

--- src/app/policy/c/layout.tsx
+++ src/app/policy/c/layout.tsx
@@ -7,7 +7,10 @@ export default function PolicyContentLayout({
 }>) {
   return (
     <>
-      <Link href='/policy'>＜戻る</Link>
+      <Link href='/policy'>
+        <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor"><path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
+        規約一覧に戻る
+      </Link>
       {children}
     </>
   );
--- src/app/wavelayout/layout.tsx
+++ src/app/wavelayout/layout.tsx
@@ -1,4 +1,5 @@
 import HomeButton from '@/app-shared/homebutton';
+import Link from 'next/link';
 
 export default function WaveLayout({
   children,
@@ -8,6 +9,11 @@ export default function WaveLayout({
   return (
     <>
       <HomeButton />
+      <Link href='/'>
+        <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor"><path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
+        一覧に戻る
+      </Link>
+      <p>※ビューが固まった場合は，ブラウザーのリロードボタンを押してください．</p>
       {children}
     </>
   );
