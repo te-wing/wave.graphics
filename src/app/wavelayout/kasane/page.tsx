cce475b4ff97733c53c789e417f4748b0c77095e
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 23:40:14 +0000

    変更

--- src/app/wavelayout/kasane/page.tsx
+++ src/app/wavelayout/kasane/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import SuperpositionCanvas from './canvas';
+import styles from './superpositionpage.module.scss'
 
 export default function 重ね合わせ() {
   // スライダの値を管理
@@ -11,39 +12,51 @@ export default function 重ね合わせ() {
   return (
     <>
       <h2>波の重ね合わせ</h2>
-      <section>
+      <section className={styles.controlSection}>
         <SuperpositionCanvas 
           waveAmplitudeLeft={leftAmplitude}
           waveAmplitudeRight={rightAmplitude}
         />
-        <div>
-          <div>
-            <label htmlFor='leftAmplitudeSlider'>
-              左の波の高さ：{leftAmplitude} px
-            </label>
-            <input
-              id='leftAmplitudeSlider'
-              type='range'
-              min='-120'
-              max='120'
-              value={leftAmplitude}
-              onChange={(e) => setLeftAmplitude(Number(e.target.value))}
-            />
-          </div>
-          <div>
-            <label htmlFor='rightAmplitudeSlider'>
-              右の波の高さ：{rightAmplitude} px
-            </label>
-            <input
-              id='rightAmplitudeSlider'
-              type='range'
-              min='-120'
-              max='120'
-              value={rightAmplitude}
-              onChange={(e) => setRightAmplitude(Number(e.target.value))}
-            />
-          </div>
-        </div>
+        <table className={styles.controlTable}>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor='leftAmplitudeSlider'>
+                  左の波の高さ：{leftAmplitude} px
+                </label>
+              </td>
+              <td>
+                <input
+                  id='leftAmplitudeSlider'
+                  className={styles.slider}
+                  type='range'
+                  min='-120'
+                  max='120'
+                  value={leftAmplitude}
+                  onChange={(e) => setLeftAmplitude(Number(e.target.value))}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor='rightAmplitudeSlider'>
+                  右の波の高さ：{rightAmplitude} px
+                </label>
+              </td>
+              <td>
+                <input
+                  id='rightAmplitudeSlider'
+                  className={styles.slider}
+                  type='range'
+                  min='-120'
+                  max='120'
+                  value={rightAmplitude}
+                  onChange={(e) => setRightAmplitude(Number(e.target.value))}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </>
   )
