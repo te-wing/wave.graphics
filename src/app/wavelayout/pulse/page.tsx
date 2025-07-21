697c5723a5e73521d8421f344de6a6855acc506d
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 13:22:05 +0000

    予定

---
+++ src/app/wavelayout/pulse/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'パルス波 | Wave App by Wing',
+  description: 'パルス波を可視化できるツールです．'
+}
+
+export default function パルス波() {
+  return(
+    <>
+      <p>すみません，準備中です...</p>
+    </>
+  )
+}
---
+++ src/app/wavelayout/sinwave/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '正弦波 | Wave App by Wing',
+  description: '正弦波を可視化できるツールです．'
+}
+
+export default function 正弦波() {
+  return(
+    <>
+      <p>すみません，準備中です...</p>
+    </>
+  )
+}
