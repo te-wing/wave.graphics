e01ddbfcce8afc7d7b8b6af689d9ae213ea66d2f
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 22:45:52 +0000

    kasane

---
+++ src/app/wavelayout/kasane/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '波の重ね合わせ | Wave App by Wing',
+  description: '先ほどのパルス波を使って，二つの波が重なったらどうなるか，観察してみましょう．'
+}
+
+export default function 重ね合わせ() {
+  return (
+    <>
+    
+    </>
+  )
+}
