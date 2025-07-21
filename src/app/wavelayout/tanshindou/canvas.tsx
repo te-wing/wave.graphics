aeeffaa0767167d19829a0b4c45b70d3823e268d
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-21 06:15:15 +0000

    できた

--- src/app/wavelayout/tanshindou/canvas.tsx
+++ src/app/wavelayout/tanshindou/canvas.tsx
@@ -180,8 +180,8 @@ const OscillationCanvas: React.FC<OscillationCanvasProps> = ({
 
   return (
     <>
-      <canvas id="myCanvas" ref={canvasRef} width={800} height={490}></canvas>
       <div className={styles.controls}>
+      <canvas id="myCanvas" ref={canvasRef} width={800} height={490}></canvas>
         <table>
           <tbody>
             <tr>
@@ -239,11 +239,11 @@ const OscillationCanvas: React.FC<OscillationCanvasProps> = ({
             </tr>
           </tbody>
         </table>
-        <div className='styles.buttonList'>
-          <button onClick={handleReset}>
+        <div className={styles.buttonList}>
+          <button className={styles.button} onClick={handleReset}>
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg>
           </button>
-          <button onClick={handlePlayPause}>
+          <button className={styles.button} onClick={handlePlayPause}>
             {isPlaying ? (
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M320-640v320-320Zm-80 400v-480h480v480H240Zm80-80h320v-320H320v320Z"/></svg>
             ) : (
