36e81c1f41b4302b64855380756aab8565d8a5a5
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-22 04:56:00 +0000

    こんなもんかな

--- src/app/wavelayout/layout.tsx
+++ src/app/wavelayout/layout.tsx
@@ -1,5 +1,6 @@
 import HomeButton from '@/app-shared/homebutton';
 import Link from 'next/link';
+import styles from './wavelayoutstyles.module.scss';
 
 export default function WaveLayout({
   children,
@@ -9,11 +10,11 @@ export default function WaveLayout({
   return (
     <>
       <HomeButton />
-      <Link href='/'>
+      <Link className={styles.menu} href='/'>
         <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor"><path d="M640-80 240-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
         一覧に戻る
       </Link>
-      <p>※ビューが固まった場合は，ブラウザーのリロードボタンを押してください．</p>
+      <p className={styles.menu}>※ビューが固まった場合は，ブラウザーのリロードボタンを押してください．</p>
       {children}
     </>
   );
