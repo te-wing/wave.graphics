2820d699a048601a661613517e17a7c64d4bc976
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-27 21:39:28 +0900

    eslint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // 他のルールを追加
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
