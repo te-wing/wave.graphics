9d22cadc0f4f5bd40486af286d06434b1bf7f5d0
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-07-27 04:09:16 +0000

    ストップボタンいらんな．

--- src/app/wavelayout/sinwave/canvas.tsx
+++ src/app/wavelayout/sinwave/canvas.tsx
@@ -328,13 +328,6 @@ const OscillationCanvas: React.FC<OscillationCanvasProps> = ({
           <button className={styles.button} onClick={handleReset}> {/* stylesモジュールを使用 */}
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg>
           </button>
-          <button className={styles.button} onClick={handlePlayPause}> {/* stylesモジュールを使用 */}
-            {isPlaying ? (
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M320-640v320-320Zm-80 400v-480h480v480H240Zm80-80h320v-320H320v320Z"/></svg>
-            ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
-            )}
-          </button>
         </div>
       </div>
     </>
