dbbb20f073218ab546bed27f811a9770cf670d40
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-12 17:56:41 +0900

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://wave.app.wing.osaka',
+  siteUrl: process.env.SITE_URL || 'https://wave.graphics',
   generateRobotsTxt: true,
   // ... その他の設定
 };
